8134c8034128bc6333d091693c2a5ba59d59006f
Salil <jayinc@users.noreply.github.com>
2022-02-10 12:12:04 -0800

    notwrkng

--- db.mjs
+++ db.mjs
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-//// mongodb connection code ////
+// mongodb connection code ////
 let dbURI =
   "mongodb+srv://atlas-monster:greek-Myth@cluster0.owvlg.mongodb.net/studentDb?retryWrites=true&w=majority";
 // let dbURI = 'mongodb://localhost/mydatabase';
 mongoose.connect(dbURI);
 
-////---START---//// Mongodb connected disconnected events ////
+// ---START---//// Mongodb connected disconnected events ////
 mongoose.connection.on("connected", function () {
   // if Connected callback
   console.log("Mongoose is connected");
@@ -19,12 +19,12 @@ mongoose.connection.on("disconnected", function () {
 });
 
 mongoose.connection.on("error", function (err) {
-  // if error callback
-  console.log("Mongoose connection error: " + err);
+  // for any error callback
+  console.log("Mongoose connection error: ", err);
   process.exit(1);
 });
 
-process.on(SIGINT, function () {
+process.on("SIGINT", function () {
   // Signal Intent function runs just before the app closes
   console.log("App is terminating");
   mongoose.connection.close(function () {
@@ -32,9 +32,9 @@ process.on(SIGINT, function () {
     process.exit(0);
   });
 });
-////---END---//// Mongodb connected disconnected events ////
+//---END---//// Mongodb connected disconnected events ////
 
-// !! Mongodb Schema ////---START---////
+// !! Mongodb Schema for Students & Attendance Collections ////---START---////
 
 // ** Schema for Student Collection ** //
 const studentSchema = new mongoose.Schema({
@@ -47,10 +47,10 @@ const studentSchema = new mongoose.Schema({
 export const student = mongoose.model("student", studentSchema);
 
 // ** Schema for Attendance Collection ** //
-const attendance = new mongoose.Schema({
+const attendanceSchema = new mongoose.Schema({
   studentId: string,
   studentName: string,
   isPresent: boolean,
   createdOn: { type: Date, default: Date.now },
 });
-export const attendance = mongoose.model("attendance", attendance.Schema);
+export const attendance = mongoose.model("attendance", attendanceSchema);
